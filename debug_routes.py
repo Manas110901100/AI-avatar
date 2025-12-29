8f0428e5b99e8a82fa963c83a82b4e992a1f1d5d
Manas Kumar <manassherla03@gmail.com>
2025-12-28 23:47:41 -0600

    Updated gitignore to exclude extra files

--- debug_routes.py
+++
@@ -1,8 +0,0 @@
-from main import app
-import json
-
-for route in app.routes:
-    if hasattr(route, "methods"):
-        print(f"Path: {route.path}, Methods: {route.methods}")
-    else:
-        print(f"Mount: {route.path}")
